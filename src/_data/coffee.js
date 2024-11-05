606e455d57a351a0a323d51299748ab5d0b0b69b
Will Rynearson <will@developmentseed.org>
2024-11-05 10:11:19 +0100

    update

--- src/_data/coffee.js
+++ src/_data/coffee.js
@@ -9,7 +9,7 @@ module.exports = async function () {
     let json = await EleventyFetch(
       "https://script.google.com/macros/s/AKfycbz6Ts5iMeBOknsiJVS6wb9F03pBqwgkXVgSbzYJioMzomUdLO2_o7RRaeznjsmzYumb/exec",
       {
-        duration: "30m",
+        duration: "5m",
         type: "json",
         // this fetches the text from the AppsScript deployment and caches it for 30 min. The text is converted to a json.
       }
