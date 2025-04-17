9cf8223fcba76c782418809d0eda7dac7c5b876c
Will Rynearson <will@developmentseed.org>
2025-04-17 15:01:26 +0200

    fix syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { fortawesomeFreeRegularPlugin } from "@vidhill/fortawesome-free-regular-11ty-shortcode";
 import moment from "moment";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 
 export default function (eleventyConfig) {
   eleventyConfig.setTemplateFormats(["md", "liquid"]);
@@ -26,6 +27,8 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
 
   eleventyConfig.addPlugin(fortawesomeFreeRegularPlugin);
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addFilter("dateFormat", async function (date) {
     return moment(date).format("MMMM Do, YYYY");
